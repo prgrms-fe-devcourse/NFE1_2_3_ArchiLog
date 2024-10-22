6c75bdefeb56e17b62d41f523fbc178b200173e5
gyu <qkr9748200@gmail.com>
2024-10-22 18:05:03 +0900

    헤더,다크모드,반응형 일부 완성

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
+import Header2 from "./components/Layout/Header2.tsx";
+import Blog from "./components/blog/Blog.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const DarkMode = () => {
+    setIsDarkMode(p => !p);
+    if (isDarkMode) {
+      document.documentElement.classList.remove("dark");
+    } else {
+      document.documentElement.classList.add("dark");
+    }
+  };
 
   return (
-    <>
-      <div>
+    <div className={`duration-300 h-screen ${isDarkMode ? "dark:bg-black" : "bg-white"}`}>
+      <Header2 isDarkMode={isDarkMode} DarkMode={DarkMode}/>
+      <Blog isDarkMode={isDarkMode}/>
+     
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -18,18 +32,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p> */}
+    </div>
+  );
 }
 
-export default App
+export default App;
