fa44c4b5cce8f8fb63639bd0655dda2b8566e2c7
gyu <qkr9748200@gmail.com>
2024-10-24 12:54:19 +0900

    오류 수정

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,13 +5,13 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
       colors: {
         headerDarkHover: "#FDAD00",
-        tagDarkHover: "#ffd52d",
+        
         Datecolor: "#F1F1F1",
         primary: {
           DEFAULT: "#2bca43",
