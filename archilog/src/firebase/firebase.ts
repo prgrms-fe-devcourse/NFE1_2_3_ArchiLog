a00da9f3c1be26bc012155b66bf89431488e84e7
Euiyeong <105384780+snsdl0905@users.noreply.github.com>
2024-10-25 17:15:58 +0900

    feat: 이력서를 저장하고 출력할 수 있다

--- archilog/src/firebase/firebase.ts
+++ archilog/src/firebase/firebase.ts
@@ -1,23 +1,28 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { getAuth } from "firebase/auth";
+// import { getAuth } from "firebase/auth";
+import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: process.env.VITE_FIREBASE_API_KEY,
   authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: "https://archilog-7b615-default-rtdb.firebaseio.com",
   projectId: process.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.VITE_FIREBASE_APP_ID,
-  measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID
+  measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
+// const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
-export const auth = getAuth(app);
+// export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const database = getDatabase(app);
+
 
 export default app;
--- archilog/src/pages/register.tsx
+++ archilog/src/pages/register.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router"; 
 import axios from 'axios'; 
-import HeaderLogin from "@/components/Layout/HeaderLogin";
+import HeaderLogin from "../components/Layout/HeaderLogin";
 
 const RegisterLayout: React.FC = () => {
   const [darkMode, setDarkMode] = useState(false);
