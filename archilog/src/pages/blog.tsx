646ec2326a9694a96331e2e73cb7686883072ef1
gyu <qkr9748200@gmail.com>
2024-10-25 17:19:45 +0900

    헤더변경

--- archilog/src/pages/blog.tsx
+++ archilog/src/pages/blog.tsx
@@ -1,25 +1,7 @@
-import { useState } from "react";
-import Header from "@/components/Layout/Header2";
 import Blog from "@/components/blog/Bloginven";
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  const DarkMode = () => {
-    setIsDarkMode((p) => !p);
-    if (isDarkMode) {
-      document.documentElement.classList.remove("dark");
-    } else {
-      document.documentElement.classList.add("dark");
-    }
-  };
-
-  return (
-    <div className={`duration-300 h-screen ${isDarkMode ? "dark:bg-black" : "bg-white"}`}>
-      <Header isDarkMode={isDarkMode} DarkMode={DarkMode} />
-      <Blog isDarkMode={isDarkMode} />
-    </div>
-  );
+  return <Blog darkMode/>  
 }
 
 export default App;
