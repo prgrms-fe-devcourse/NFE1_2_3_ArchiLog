cfc59de922b958f6e47c6302a91e3a1979f129dc
verynicetomato <verynicetomato@naver.com>
2024-10-30 16:30:55 +0900

    로그인 및 회원가입 오류 해결 + 비밀번호 찾기 구현

--- archilog/src/firebase/users.ts
+++ archilog/src/firebase/users.ts
@@ -1,15 +1,14 @@
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { getDatabase, ref, get, update, child } from "firebase/database";
+import { ref, get, update } from "firebase/database";
 import { database } from "./firebase";
-import { getCurrentUserId } from "./auth";
-
 
 // 현재 사용자 정보
-export const getCurrentUserInfo = () => {
-  return new Promise(async (resolve, reject) => {
+export const getCurrentUserInfo = (): Promise<any | null> => {
+  return new Promise((resolve) => {
     const auth = getAuth();
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
       unsubscribe();
+
       if (user) {
         try {
           const userRef = ref(database, `users/${user.uid}`);
@@ -18,19 +17,15 @@ export const getCurrentUserInfo = () => {
           if (snapshot.exists()) {
             resolve(snapshot.val());
           } else {
-            reject(new Error("No user data found"));
+            console.error("No user data found for authenticated user.");
+            resolve(null);
           }
         } catch (error) {
-          let errorMessage;
-          if (error instanceof Error){
-            errorMessage = error.message
-          } else {
-            errorMessage = String(error)
-          }
-          reject(new Error("데이터를 불러오는 중 오류 발생: " + errorMessage));
+          console.error("데이터를 불러오는 중 오류 발생:", error);
+          resolve(null);
         }
       } else {
-        reject(new Error("User is not authenticated"));
+        resolve(null);
       }
     });
   });
@@ -44,7 +39,7 @@ export const getAbout = async (userId: string) => {
     const snapshot = await get(userRef);
     if (snapshot.exists()) {
       const userData = snapshot.val();
-      console.log(userData)
+      console.log(userData);
       return {
         id: userData.userId,
         name: userData.username,
@@ -59,19 +54,13 @@ export const getAbout = async (userId: string) => {
   }
 };
 
-//마이페이지 수정 함수
+// 마이페이지 수정 함수
 export const editAbout = async (resume: string) => {
   const auth = getAuth();
   const user = auth.currentUser;
-  const userId = user?.uid;
-  console.log(user);
 
   if (!user) {
     throw new Error("User is not authenticated");
-  }
-
-  if (user.uid !== userId) {
-    throw new Error("Unauthorized access");
   }
 
   const userRef = ref(database, `users/${user.uid}`);
