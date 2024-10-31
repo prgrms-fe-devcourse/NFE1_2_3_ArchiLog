d321cf4b57a10b0b056642990fd4529f4e93d12e
HareuBang <divjae7124@gmail.com>
2024-10-31 23:15:56 +0900

    style: 게시판 상세 페이지 디자인 변경(목록)

--- archilog/src/pages/[username]/blog/[postId].tsx
+++ archilog/src/pages/[username]/blog/[postId].tsx
@@ -42,10 +42,9 @@ const PostDetail = () => {
       const postData = await getPostDetails(username, postId);
       setPost(postData);
 
-      // 타임스탬프를 날짜로 변환
       const commentsArray = Object.entries(postData.comments || {}).map(
         ([id, comment]) => {
-          const createdAtDate = new Date(comment.createdAt); // 타임스탬프를 Date 객체로 변환
+          const createdAtDate = new Date(comment.createdAt);
           const formattedDate = new Intl.DateTimeFormat("ko-KR", {
             year: "numeric",
             month: "2-digit",
@@ -55,7 +54,7 @@ const PostDetail = () => {
           return {
             id,
             ...comment,
-            createdAt: formattedDate, // 포맷팅된 날짜 사용
+            createdAt: formattedDate,
           };
         }
       );
@@ -94,7 +93,7 @@ const PostDetail = () => {
     if (confirmDelete) {
       try {
         await deletePost(postId as string);
-        router.push(`/${user?.displayName}/blog`); // 게시글 목록 페이지로 이동
+        router.push(`/${user?.displayName}/blog`);
       } catch (error) {
         console.error("Failed to delete post:", error);
       }
@@ -102,7 +101,7 @@ const PostDetail = () => {
   };
 
   const handleUpdatePost = () => {
-    router.push(`/${user?.displayName}/blog/edit/${postId}`); // 수정 페이지로 이동
+    router.push(`/${user?.displayName}/blog/edit/${postId}`);
   };
 
   const handleDeleteComment = async (commentId: string) => {
@@ -136,128 +135,39 @@ const PostDetail = () => {
   };
 
   return (
-    <div
-      className={`max-w-4xl mx-auto mt-8 p-6 rounded-lg shadow-md ${
-        darkMode ? "bg-gray-900 text-gray-100" : "bg-white text-gray-900"
-      }`}
-    >
-      {/* 게시글 제목 */}
+    <div className="max-w-4xl mx-auto mt-8 p-6 rounded-lg shadow-md">
       {post && (
-        <div className="flex flex-col lg:flex-row">
-          {/* Left Content */}
-          <div className="flex-grow">
-            <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
-
-            <div className="flex items-center gap-2 mb-6">
-              {post.tags.map((tag: string, index: number) => (
-                <span
-                  key={index}
-                  className={`text-sm px-2 py-1 rounded-full ${
-                    darkMode
-                      ? "bg-gray-800 text-gray-400"
-                      : "bg-gray-200 text-gray-700"
-                  }`}
-                >
-                  #{tag}
-                </span>
-              ))}
-            </div>
-
-            {/* 게시글 내용 */}
-            <div
-              className={`prose ${
-                darkMode ? "prose-invert" : ""
-              } max-w-none mb-10`}
-              ref={postContentRef}
-              dangerouslySetInnerHTML={{ __html: post.content }}
-            />
-
-            {/* 댓글 */}
-            <div className="mt-8 space-y-4">
-              {user ? (
-                <div
-                  className={`flex items-center rounded-full px-4 py-3 mt-4 ${
-                    darkMode
-                      ? "bg-gray-800 border-gray-700"
-                      : "bg-gray-100 border-gray-300"
-                  }`}
-                >
-                  <input
-                    type="text"
-                    placeholder="Add a comment"
-                    value={commentText}
-                    onChange={(e) => setCommentText(e.target.value)}
-                    className="bg-transparent flex-grow outline-none"
-                  />
-                  <button
-                    onClick={handleAddComment}
-                    className="focus:outline-none"
-                    aria-label="Submit comment"
-                  >
-                    {/* SVG for Paper Airplane Icon */}
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-5 h-5 transform rotate-45"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M3 10l9-9m0 0l9 9m-9-9v18"
-                      />
-                    </svg>
-                  </button>
-                </div>
-              ) : (
-                <p className="text-gray-500">
-                  로그인 후 댓글 작성이 가능합니다.
-                </p>
-              )}
-              <ul className="space-y-4">
-                {comments.map((comment) => (
-                  <li
-                    key={comment.id}
-                    className={`p-4 rounded-lg shadow-md border ${
-                      darkMode
-                        ? "bg-gray-800 border-gray-700 text-gray-300"
-                        : "bg-gray-100 border-gray-300 text-gray-800"
-                    }`}
-                  >
-                    <div>
-                      <span className="font-semibold">작성자 이름</span>{" "}
-                      <span className="text-sm">{comment.createdAt}</span>
-                    </div>
-                    <p>{comment.content}</p>
-                    {user?.uid === comment.authorId && (
-                      <div className="flex justify-end mt-2">
-                        <button
-                          onClick={() => handleDeleteComment(comment.id)}
-                          className="text-red-500 hover:underline"
-                        >
-                          삭제
-                        </button>
-                      </div>
-                    )}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-
-          {/* 목차 */}
+        <div className="flex flex-col lg:flex-row-reverse">
+          {/* 목차 for Mobile */}
           <aside
-            className={`ml-0 lg:ml-8 mt-6 lg:mt-0 lg:w-1/4 rounded-lg border ${
+            className={`lg:hidden rounded-lg border ${
               darkMode
                 ? "bg-gray-800 border-gray-600"
                 : "bg-gray-100 border-gray-300"
-            } p-4`}
+            } p-4 mb-6`}
           >
-            <div className="flex items-center justify-between">
-              <h2 className="text-xl font-semibold">목차</h2>
-              {user?.uid === post.authorId && (
+            <h2 className="text-xl font-semibold">목차</h2>
+            <ul className="space-y-1 mt-4 max-h-[60vh] overflow-y-auto">
+              {generateTableOfContents().map((item) => (
+                <li
+                  key={item.id}
+                  className={`cursor-pointer text-sm ${
+                    darkMode ? "text-blue-400" : "text-blue-600"
+                  } hover:underline pl-${
+                    item.level === "H2" ? "4" : "8"
+                  } overflow-hidden whitespace-nowrap text-ellipsis`}
+                  onClick={() => handleScrollToSection(item.id)}
+                >
+                  {item.text}
+                </li>
+              ))}
+            </ul>
+          </aside>
+
+          {/* Left Content */}
+          <div className="flex-grow ml-4">
+            {user?.uid === post.authorId && (
+              <div className="flex justify-end mb-2">
                 <div className="relative">
                   <button
                     onClick={() => setShowDropdown(!showDropdown)}
@@ -282,15 +192,53 @@ const PostDetail = () => {
                     </div>
                   )}
                 </div>
-              )}
+              </div>
+            )}
+
+            <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
+
+            <div className="flex items-center gap-2 mb-6">
+              {post.tags.map((tag: string, index: number) => (
+                <span
+                  key={index}
+                  className={`text-sm px-2 py-1 rounded-full ${
+                    darkMode
+                      ? "bg-gray-800 text-gray-400"
+                      : "bg-gray-200 text-gray-700"
+                  }`}
+                >
+                  #{tag}
+                </span>
+              ))}
             </div>
-            <ul className="space-y-1 mt-4">
+
+            <div
+              className={`prose ${
+                darkMode ? "prose-invert" : ""
+              } max-w-none mb-10`}
+              ref={postContentRef}
+              dangerouslySetInnerHTML={{ __html: post.content }}
+            />
+          </div>
+
+          {/* 목차 for Desktop */}
+          <aside
+            className={`hidden lg:block lg:sticky lg:top-24 lg:h-auto lg:max-h-screen overflow-auto lg:w-80 rounded-lg border ${
+              darkMode
+                ? "bg-gray-800 border-gray-600"
+                : "bg-gray-100 border-gray-300"
+            } p-4 mb-6 lg:mb-0`}
+          >
+            <h2 className="text-xl font-semibold">목차</h2>
+            <ul className="space-y-1 mt-4 max-h-[60vh] overflow-y-auto">
               {generateTableOfContents().map((item) => (
                 <li
                   key={item.id}
                   className={`cursor-pointer text-sm ${
                     darkMode ? "text-blue-400" : "text-blue-600"
-                  } hover:underline pl-${item.level === "H2" ? "4" : "8"}`}
+                  } hover:underline pl-${
+                    item.level === "H2" ? "4" : "8"
+                  } overflow-hidden whitespace-nowrap text-ellipsis`}
                   onClick={() => handleScrollToSection(item.id)}
                 >
                   {item.text}
@@ -300,6 +248,76 @@ const PostDetail = () => {
           </aside>
         </div>
       )}
+      {/* 댓글 작성 및 댓글 목록 */}
+      <div className="mt-8 space-y-4">
+        {user ? (
+          <div
+            className={`flex items-center rounded-full px-4 py-3 mt-4 ${
+              darkMode
+                ? "bg-gray-800 border-gray-700"
+                : "bg-gray-100 border-gray-300"
+            }`}
+          >
+            <input
+              type="text"
+              placeholder="Add a comment"
+              value={commentText}
+              onChange={(e) => setCommentText(e.target.value)}
+              className="bg-transparent flex-grow outline-none"
+            />
+            <button
+              onClick={handleAddComment}
+              className="focus:outline-none"
+              aria-label="Submit comment"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-5 h-5 transform rotate-45"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3 10l9-9m0 0l9 9m-9-9v18"
+                />
+              </svg>
+            </button>
+          </div>
+        ) : (
+          <p className="text-gray-500">로그인 후 댓글 작성이 가능합니다.</p>
+        )}
+        <ul className="space-y-4">
+          {comments.map((comment) => (
+            <li
+              key={comment.id}
+              className={`p-4 rounded-lg shadow-md border ${
+                darkMode
+                  ? "bg-gray-800 border-gray-700 text-gray-300"
+                  : "bg-gray-100 border-gray-300 text-gray-800"
+              }`}
+            >
+              <div>
+                <span className="font-semibold">작성자 이름</span>{" "}
+                <span className="text-sm">{comment.createdAt}</span>
+              </div>
+              <p>{comment.content}</p>
+              {user?.uid === comment.authorId && (
+                <div className="flex justify-end mt-2">
+                  <button
+                    onClick={() => handleDeleteComment(comment.id)}
+                    className="text-red-500 hover:underline"
+                  >
+                    삭제
+                  </button>
+                </div>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
